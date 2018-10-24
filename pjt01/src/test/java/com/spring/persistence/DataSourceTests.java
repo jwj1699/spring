42e5710383bbaddaf59090f627ab65fe1a4079d1
DESKTOP-N41D6TJ\WOOJIN <jwj1699@gmail.com>
2018-10-24 22:34:47 +0900

    MyBatis 테스트

--- pjt01/src/test/java/com/spring/persistence/DataSourceTests.java
+++ pjt01/src/test/java/com/spring/persistence/DataSourceTests.java
@@ -2,6 +2,8 @@ package com.spring.persistence;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -13,6 +15,8 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import static org.junit.Assert.fail;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/root-context.xml")
 @Log4j
@@ -21,13 +25,29 @@ public class DataSourceTests {
     @Setter(onMethod_ = {@Autowired})
     private DataSource dataSource;
 
+    @Setter(onMethod_ = {@Autowired})
+    private SqlSessionFactory sqlSessionFactory;
+
     @Test
     public void testConnection(){
 
         try(Connection con = dataSource.getConnection()){
             log.info(con);
         } catch (SQLException e) {
-            e.printStackTrace();
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testMyBaits(){
+
+        try(SqlSession session = sqlSessionFactory.openSession();
+            Connection con = session.getConnection()) {
+
+            log.info(session);
+            log.info(con);
+        } catch (SQLException e) {
+            fail(e.getMessage());
         }
     }
 }
