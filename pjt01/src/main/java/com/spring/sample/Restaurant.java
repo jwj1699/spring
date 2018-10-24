f5cb65e6ceb8527cff1d07b13f1ffb05ea0d248b
DESKTOP-N41D6TJ\WOOJIN <jwj1699@gmail.com>
2018-10-24 22:25:17 +0900

    JDBC와 커넥션풀 테스트

--- pjt01/src/main/java/com/spring/sample/Restaurant.java
+++ pjt01/src/main/java/com/spring/sample/Restaurant.java
@@ -1,14 +1,13 @@
 package com.spring.sample;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @Data
+@AllArgsConstructor
 public class Restaurant {
 
-    @Setter(onMethod_ = {@Autowired})
     private Chef chef;
 }
