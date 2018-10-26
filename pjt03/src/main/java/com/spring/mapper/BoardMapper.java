5d6e0e4c231c0e1965451e3e229b311521c0a600
DESKTOP-N41D6TJ\WOOJIN <jwj1699@gmail.com>
2018-10-26 19:58:50 +0900

    영속 영역의 CRUD 구현

--- pjt03/src/main/java/com/spring/mapper/BoardMapper.java
+++ pjt03/src/main/java/com/spring/mapper/BoardMapper.java
@@ -9,4 +9,15 @@ public interface BoardMapper {
 
     public List<BoardVO> getList();
 
+    //insert만 처리되고 생성된 PK값을 알 필요가 없는 경우
+    public void insert(BoardVO board);
+
+    //insert가 실행되고 생성된 PK 값을 알야야 하는 경우
+    public void  insertSelectKey(BoardVO board);
+
+    public BoardVO read(Long bno);
+
+    public int delete(Long bno);
+
+    public int update(BoardVO board);
 }
