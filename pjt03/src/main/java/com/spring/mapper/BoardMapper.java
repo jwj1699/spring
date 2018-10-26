fab91fbfa186f4ed9c8484c2036480991f48c41c
DESKTOP-N41D6TJ\WOOJIN <jwj1699@gmail.com>
2018-10-26 18:59:34 +0900

    (테스트) mapper

---
+++ pjt03/src/main/java/com/spring/mapper/BoardMapper.java
@@ -0,0 +1,12 @@
+package com.spring.mapper;
+
+
+import com.spring.domain.BoardVO;
+
+import java.util.List;
+
+public interface BoardMapper {
+
+    public List<BoardVO> getList();
+
+}
---
+++ pjt03/src/test/java/com/spring/mapper/BoardMapperTests.java
@@ -0,0 +1,23 @@
+package com.spring.mapper;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/root-context.xml")
+@Log4j
+public class BoardMapperTests {
+
+    @Setter(onMethod_ = {@Autowired})
+    private BoardMapper mapper;
+
+    @Test
+    public void testGetList(){
+        mapper.getList().forEach(board -> log.info(board));
+    }
+}
