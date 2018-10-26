57a4f8b9d7adda46864180f547a6cde7fcbb5f35
DESKTOP-N41D6TJ\WOOJIN <jwj1699@gmail.com>
2018-10-26 14:38:18 +0900

    컨트롤러 리턴타입 (메이븐 추가 jackson)

--- pjt02/src/main/java/com/spring/pjt02/HomeController.java
+++ pjt02/src/main/java/com/spring/pjt02/HomeController.java
@@ -4,9 +4,11 @@ import com.spring.domain.SampleDTO;
 import com.spring.domain.SampleDTOList;
 import com.spring.domain.TodoDTO;
 import lombok.extern.log4j.Log4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -68,5 +70,60 @@ public class HomeController {
         log.info("todo=" + dto);
 
         return "dto";
+    }
+
+    //Java Beans 규칙에 맞는 객체는 다시 화면으로 전달된다, 기본자료형은 전달이 안됨
+    // (/sample/test07?name=aaa&age=11&page=9)
+    @RequestMapping("/test07")
+    public String test07(SampleDTO dto, int page){
+
+        log.info("dto=" + dto);
+        log.info("page=" + page);
+
+        return "test07";
+    }
+
+    //기본자료형을 파라미터로 뷰에 보내는 방법
+    // (/sample/test08?name=aaa&age=11&page=9)
+    @RequestMapping("/test08")
+    public String test08(SampleDTO dto,@ModelAttribute("page") int page){
+
+        log.info("dto=" + dto);
+        log.info("page=" + page);
+
+        return "test07";
+    }
+
+    //(void 리턴타입) 일경우 url의 경로로 jsp파일을 사용
+    // (/sample/test09)
+    @RequestMapping("/test09")
+    public void test09(){
+
+        log.info("test09................");
+    }
+
+    //(객체 리턴타입) jackson-databind 라이브러리사용시 json데이터로 리턴
+    // (/sample/test10)
+    @RequestMapping("/test10")
+    public @ResponseBody SampleDTO test10(){
+
+        SampleDTO dto = new SampleDTO();
+        dto.setAge(11);
+        dto.setName("홍길동");
+
+        return dto;
+    }
+
+    //ResponseEntity를 사용하여 헤더정보나 데이터 전달 가능
+    // (/sample/test11)
+    @RequestMapping("/test11")
+    public ResponseEntity<String> test11(){
+
+        String msg = "{\"name\": \"홍길동\"}";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json;charset=UTF-8");
+
+        return new ResponseEntity<>(msg, headers, HttpStatus.OK);
     }
 }
---
+++ pjt02/src/main/webapp/WEB-INF/views/test07.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Test07</title>
+</head>
+<body>
+    <h2>SampleDTO ${sampleDTO}</h2>
+    <h2>page ${page}</h2>
+</body>
+</html>
