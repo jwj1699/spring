fde6cff426eb1c0993aa6ce5c0608bb094c8dfbc
DESKTOP-N41D6TJ\WOOJIN <jwj1699@gmail.com>
2018-11-04 00:55:09 +0900

    프로젝트 생성 및 기본환경구성 메이븐(aspectj + lombok + junit + log4j + log4jdbc-log4j2 + mybatis + HikariCP)

---
+++ pjt05/src/main/java/com/spring/pjt05/HomeController.java
@@ -0,0 +1,13 @@
+package com.spring.pjt05;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String home(){
+        return"index";
+    }
+}
